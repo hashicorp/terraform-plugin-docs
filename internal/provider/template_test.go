7fe092fb4f56bba4e743018a79255807fa88b8fd
Andre Moeller <andre.moeller@datadrivers.de>
2022-07-04 08:11:48 +0200

    fix(template functions): Use function Title instead of ToTitle to get Capitalize strings instead of upper strings
    using cases.Title because the strings.Title function is deprecated and refs to cases.Title

---
+++ internal/provider/template_test.go
@@ -0,0 +1,47 @@
+package provider
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestRenderStringTemplate(t *testing.T) {
+	template := `
+Plainmarkdown: {{ plainmarkdown .Text }}
+Split: {{ $arr := split .Text " "}}{{ index $arr 3 }}
+Trimspace: {{ trimspace .Text }}
+Lower: {{ upper .Text }}
+Upper: {{ lower .Text }}
+Title: {{ title .Text }}
+Prefixlines:
+{{ prefixlines "  " .MultiLineTest }}
+`
+
+	expectedString := `
+Plainmarkdown: my Odly cAsed striNg
+Split: striNg
+Trimspace: my Odly cAsed striNg
+Lower: MY ODLY CASED STRING
+Upper: my odly cased string
+Title: My Odly Cased String
+Prefixlines:
+  This text used
+  multiple lines
+`
+	result, err := renderStringTemplate("testTemplate", template, struct {
+		Text          string
+		MultiLineTest string
+	}{
+		Text: "my Odly cAsed striNg",
+		MultiLineTest: `This text used
+multiple lines`,
+	})
+
+	if err != nil {
+		t.Error(err)
+	}
+	if !cmp.Equal(expectedString, result) {
+		t.Errorf("expected: %+v, got: %+v", expectedString, result)
+	}
+}
