05a435933cc80d11e36d5dce032919bb094c33d0
Selena Goods <github@simplebox.anonaddy.com>
2023-11-16 18:41:33 -0500

    Add support for `--providers-schema` flag to pass in a providers schema JSON file.

---
+++ internal/provider/generate_test.go
@@ -0,0 +1,42 @@
+package provider
+
+import (
+	"testing"
+
+	"github.com/mitchellh/cli"
+)
+
+func TestGenerator_terraformProviderSchemaFromFile(t *testing.T) {
+	t.Parallel()
+
+	g := &generator{
+		ignoreDeprecated: true,
+		tfVersion:        "1.0.0",
+
+		providerDir:         "testdata/test-provider-dir",
+		providerName:        "terraform-provider-null",
+		providersSchemaPath: "testdata/schema.json",
+		ui:                  cli.NewMockUi(),
+	}
+
+	providerSchema, err := g.terraformProviderSchemaFromFile()
+	if err != nil {
+		t.Fatalf("error retrieving schema: %q", err)
+	}
+
+	if providerSchema == nil {
+		t.Fatalf("provider schema not found")
+	}
+	if providerSchema.ResourceSchemas["null_resource"] == nil {
+		t.Fatalf("null_resource not found")
+	}
+	if providerSchema.DataSourceSchemas["null_data_source"] == nil {
+		t.Fatalf("null_data_source not found")
+	}
+	if providerSchema.ResourceSchemas["null_resource"].Block.Attributes["id"] == nil {
+		t.Fatalf("null_resoruce id attribute not found")
+	}
+	if providerSchema.DataSourceSchemas["null_data_source"].Block.Attributes["id"] == nil {
+		t.Fatalf("null_data_source id attribute not found")
+	}
+}
