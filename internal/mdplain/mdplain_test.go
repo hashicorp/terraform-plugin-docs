cf8ac2c70f881e046a8a0caa42b3687790dc7f5e
Selena Goods <github@simplebox.anonaddy.com>
2024-01-25 15:58:06 -0500

    Add test for `PlainMarkdown()`

---
+++ internal/mdplain/mdplain_test.go
@@ -0,0 +1,35 @@
+package mdplain
+
+import (
+	"os"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestPlainMarkdown(t *testing.T) {
+	t.Parallel()
+
+	input, err := os.ReadFile("testdata/markdown.md")
+	if err != nil {
+		t.Errorf("Error opening file: %s", err.Error())
+		return
+	}
+
+	expectedFile, err := os.ReadFile("testdata/mdplain.txt")
+	if err != nil {
+		t.Errorf("Error opening file: %s", err.Error())
+		return
+	}
+
+	expected := string(expectedFile)
+	actual, err := PlainMarkdown(string(input))
+	if err != nil {
+		t.Errorf("Error rendering markdown: %s", err.Error())
+		return
+	}
+	if !cmp.Equal(expected, actual) {
+		t.Errorf(cmp.Diff(expected, actual))
+	}
+
+}
